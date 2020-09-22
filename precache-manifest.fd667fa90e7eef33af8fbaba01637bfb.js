fbaaac0fbb843df6ee916728ea4434d73a7edf1b
Mateusz Mikulski <mikulskee@gmail.com>
2020-09-22 19:20:35 +0200

    Updates

---
+++ precache-manifest.fd667fa90e7eef33af8fbaba01637bfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5efbcc184a0f66342ac32afab2db8b8",
+    "url": "/chart-app/index.html"
+  },
+  {
+    "revision": "09f2e1fe2c15ac211e5e",
+    "url": "/chart-app/static/js/2.9faaafe3.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/chart-app/static/js/2.9faaafe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8457832392c6447ae6",
+    "url": "/chart-app/static/js/main.25b6895b.chunk.js"
+  },
+  {
+    "revision": "c515bba2ccdd7aea6d89",
+    "url": "/chart-app/static/js/runtime-main.e775e2a4.js"
+  },
+  {
+    "revision": "78d6b6c621deda320b6867cdcb4041da",
+    "url": "/chart-app/static/media/covid-photo-2.78d6b6c6.jpg"
+  },
+  {
+    "revision": "987d58a56b010ea14e3b824f9e49af97",
+    "url": "/chart-app/static/media/covid-photo-3.987d58a5.jpg"
+  },
+  {
+    "revision": "a42fccb25c1f1a914087f1fddf541e8b",
+    "url": "/chart-app/static/media/covid-photo.a42fccb2.jpg"
+  }
+]);
